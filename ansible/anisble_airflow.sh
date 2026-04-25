fcac3bb17f2862f11083d47deecc5afa302302df
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2026-04-25 16:25:24 +0100

    updated username for airflow container account

--- ansible/anisble_airflow.sh
+++ ansible/anisble_airflow.sh
@@ -2,22 +2,22 @@
 apt update && apt install -y openssh-server python3 sudo
 systemctl enable --now ssh
 
-useradd -m -s /bin/bash ansible_admin -G sudo
-echo "ansible_admin:<PASSWORD>" | chpasswd
+useradd -m -s /bin/bash airflow_admin -G sudo
+echo "airflow_admin:<PASSWORD>" | chpasswd
 
 mkdir -p /etc/sudoers.d/
-echo "ansible_admin ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ansible
+echo "airflow_admin ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ansible
 chmod 0440 /etc/sudoers.d/ansible
 
-mkdir -p /home/ansible_admin/.ssh
-chown ansible_admin:ansible_admin /home/ansible_admin/.ssh
-chmod 700 /home/ansible_admin/.ssh
+mkdir -p /home/airflow_admin/.ssh
+chown airflow_admin:airflow_admin /home/airflow_admin/.ssh
+chmod 700 /home/airflow_admin/.ssh
 
 # ANSIBLE CONTAINER (CONTROLLER NODE)
 ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
-ssh-copy-id ansible_admin@<ip_address_of_managed_node>
-ssh ansible_admin@<ip_address_of_managed_node>
+ssh-copy-id airflow_admin@<ip_address_of_managed_node>
+ssh airflow_admin@<ip_address_of_managed_node>
 
 # Run this on the Controller if the IP was used previously
 ssh-keygen -f "/root/.ssh/known_hosts" -R "<ip_address_of_managed_node>"
-ssh-copy-id ansible_admin@<ip_address_of_managed_node>
+ssh-copy-id airflow_admin@<ip_address_of_managed_node>

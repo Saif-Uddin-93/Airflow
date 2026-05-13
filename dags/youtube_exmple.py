7a2ce4249de75dc6c9468254e7a0577ebdb70462
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2026-05-13 10:16:34 +0100

    updated youtube example

---
+++ dags/youtube_exmple.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from airflow.decorators import task, dag
+from airflow.operators.bash import BashOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
+}
+
+# with DAG(
+#     dag_id="my_first_dag",
+#     default_args=default_args,
+#     description="My first DAG",
+#     start_date=datetime(2023, 1, 1, 2),
+#     schedule_interval='@daily'
+# ) as dag:
+#     task1 = BashOperator(
+#         task_id='first_task',
+#         bash_command="echo Hello World!"
+#     )
+
+@dag(
+    dag_id="my_first_dag",
+    default_args=default_args,
+    description="My first DAG",
+    start_date=datetime(2023, 1, 1, 2),
+    schedule_interval='@daily'
+) 
+def my_first_dag():
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command="echo Hello World!"
+    )
+my_first_dag = my_first_dag()

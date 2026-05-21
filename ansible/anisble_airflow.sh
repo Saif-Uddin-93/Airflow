e896b70bd56fb89c16b2f99557f2cb9d81ae43c1
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2026-05-21 14:20:39 +0100

    updated locale

--- ansible/anisble_airflow.sh
+++ ansible/anisble_airflow.sh
@@ -16,7 +16,7 @@ chmod 700 /home/airflow_admin/.ssh
 
 # sudo apt install -y build-essential libpq-dev python3-dev
 # sudo locale-gen en_US.UTF-8
-# sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
+# sudo update-locale LANG=C.UTF-8 LC_ALL=C.UTF-8
 
 ############################################################################
 # ANSIBLE CONTAINER (CONTROLLER NODE)

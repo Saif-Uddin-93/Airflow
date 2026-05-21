3a98f25068e584a14ffd33db706554da16ee0fc9
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2026-05-21 13:39:18 +0100

    added commands for airflow server

--- ansible/anisble_airflow.sh
+++ ansible/anisble_airflow.sh
@@ -15,6 +15,8 @@ chown airflow_admin:airflow_admin /home/airflow_admin/.ssh
 chmod 700 /home/airflow_admin/.ssh
 
 # sudo apt install -y build-essential libpq-dev python3-dev
+# sudo locale-gen en_US.UTF-8
+# sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
 ############################################################################
 # ANSIBLE CONTAINER (CONTROLLER NODE)
